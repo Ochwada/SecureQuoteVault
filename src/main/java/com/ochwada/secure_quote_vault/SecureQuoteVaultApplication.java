800df7e1a430817895355743f16ac83fbfcfa530
ochwada <nakhulolinda@yahoo.com>
2025-07-25 09:44:58 +0200

    Initial project commit

---
+++ src/main/java/com/ochwada/secure_quote_vault/SecureQuoteVaultApplication.java
@@ -0,0 +1,41 @@
+package com.ochwada.secure_quote_vault;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureQuoteVaultApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecureQuoteVaultApplication.class, args);
+    }
+
+    static {
+        // Load environment variables from .env file
+        // Ignores file if missing (useful for production environments like Heroku)
+        Dotenv dotenv = Dotenv.configure()
+                .ignoreIfMissing()
+                .load();
+
+        // List of expected keys to load from the .env file
+        String[] envVars =
+                {
+                        "PORT",
+                        "MONGODB_URI",
+                        "MONGODB_DATABASE",
+                };
+        // Iterate through keys and set them as JVM system properties if found
+        for (String key : envVars) {
+            String value = dotenv.get(key);
+
+            if (value != null) {
+                System.setProperty(key, value);  // Makes it accessible via System.getProperty
+                System.out.println("✅ " + key + " loaded and set.");
+            } else {
+                System.out.println("⚠️" + key + " not found in .env file. Skipping System.");
+            }
+        }
+    }
+
+}
